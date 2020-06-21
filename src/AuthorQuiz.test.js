bb48a88e217092099f57303af5ded603d0cd36b8
unknown <elliot.redhead@gmail.com>
2020-06-21 21:11:21 +0100

    Created basic tests.

--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -1,9 +1,31 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+function Hello(props){
+  return <h1>Hello at {props.now}</h1>;
+}
+
+const moment = new Date(1588946400000);
+
+describe("When testing directly", () => {
+  let result;
+  beforeAll(() => {
+    result = Hello({now: moment.toISOString});
+  });
+  it("returns a value", () => {
+    expect(result).not.toBeNull();
+  });
+  it("is a h1 component", () => {
+    expect(result.type).toBe("h1");
+  });
+  it("has children", () => {
+    expect(result.props.children).toBeTruthy();
+  });
+});
+
+describe("When testing with ReactDOM", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Hello now={moment.toISOString()} />, div);
+  });
 });
