b85ba33db35addf9d4fe0f7bd74c30f5c48f8ccd
unknown <elliot.redhead@gmail.com>
2020-06-23 10:51:40 +0100

    Added jumbotron and footer to author quiz.

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -2,12 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function AuthorQuiz() {
-  return (
-    <div>
-      Author Quiz
+const Hero = () => (
+  <div className="row">
+    <div className="jumbotron col-10 offset-1">
+      <h1>Author Quiz</h1>
+      <p>Select the book written by the author shown.</p>
     </div>
-  );
-}
+  </div>
+);
+
+const Turn = () => (
+  <div></div>
+);
+
+const Continue = () => (
+  <div></div>
+);
+
+const Footer = () => (
+  <div id="footer" className="row">
+    <div className="col-12">
+      <p className="text-muted credit">All images are from <a href="https://commons.wikimedia.org/wiki/Main_Page">Wikimedia Commons</a> and are in the public domain.</p>
+    </div>
+  </div>
+);
+
+const AuthorQuiz = () => (
+  <div className="container-fluid">
+    <Hero />
+    <Turn />
+    <Continue />
+    <Footer />
+  </div>
+);
 
 export default AuthorQuiz;
