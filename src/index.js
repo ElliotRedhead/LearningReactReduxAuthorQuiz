aa09533cd344085f2a84afc05bcd1db8d2fb6918
unknown <elliot.redhead@gmail.com>
2020-06-23 11:04:46 +0100

    Created author obj & state. Passed to component.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,25 @@ import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
 
+const authors = [
+  {
+    name: "Mark Twain",
+    imageURL: "images/authors/marktwain.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ["The Adventures of Huckleberry Finn"]
+  }
+];
+
+const state = {
+  turnData: {
+    author: authors[0],
+    books: authors[0].books
+  }
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <AuthorQuiz />
+    <AuthorQuiz {...state}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
