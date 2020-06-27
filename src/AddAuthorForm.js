9c0952958b17add0eae66b679d9adaf4fb70812d
unknown <elliot.redhead@gmail.com>
2020-06-27 22:19:01 +0100

    Added React hooks to display text input to fields.

--- src/AddAuthorForm.js
+++ src/AddAuthorForm.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import "./AddAuthorForm.css";
 
-const AddAuthorForm = ({match}) => (
-  <div className="AddAuthorForm">
-    <h1>Add Author</h1>
-    <form>
-      <div className="AddAuthorForm__input">
-        <label htmlFor="name">Name</label>
-        <input type="text" name="name"></input>
-      </div>
-    </form>
-  </div>
-);
+const AddAuthorForm = ({match}) => {
+  const [name, setName] = React.useState("");
+  const [imageUrl, setImageUrl] = React.useState("");
+
+  return(
+    <div className="AddAuthorForm">
+      <h1>Add Author</h1>
+      <form>
+        <div className="AddAuthorForm__input">
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" value={name} onChange={event => setName(event.target.value)}></input>
+        </div>
+        <div className="AddAuthorForm__input">
+          <label htmlFor="imageUrl">Image URL</label>
+          <input type="text" name="imageUrl" value={imageUrl} onChange={event => setImageUrl(event.target.value)}></input>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default AddAuthorForm;
